6cefc7cfc4334c97522d916a2ca2695df529b6a3
jc282222 <alexander.silva@my.jcu.edu.au>
2016-05-27 16:50:17 +1000

    final update of project

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 __author__ = 'Alex Silva'
 
-
 """
 Application created by Alex Silva
 08/05/16
@@ -29,7 +28,7 @@ from itemlist import ItemList
 from AlexSilvaA1 import load_items
 from AlexSilvaA1 import save_items
 
-#Defines constants
+# Defines constants
 LIST_MODE = 0
 HIRE_MODE = 1
 RETURN_MODE = 3
@@ -64,7 +63,7 @@ class EquipmentHire(App):
         # get items from self.itemlist
         # call save functioon from AlexSilva1
         pass
-        # save_items(self.itemlist)
+        # save_items()
 
     def press_add(self):
         """
@@ -97,59 +96,86 @@ class EquipmentHire(App):
         name = instance.text
         clicked_item = self.itemlist.get_item(name)
 
-        #Checks mode selected
+        # Checks mode selected
         if self.mode == LIST_MODE:
             # update status text
-            self.status_text = "{} ({}), ${} is {}".format(name, clicked_item.description, clicked_item.cost, clicked_item.status)
+            self.status_text = "{} ({}), ${} is {}".format(name, clicked_item.description, clicked_item.cost,
+                                                           clicked_item.status)
         elif self.mode == HIRE_MODE:
+            new_cost = 0
             if clicked_item.name not in self.selected_items and clicked_item.status == 'in':
                 self.selected_items.append(clicked_item)
-                #set instance state
+                # set instance state
                 instance.state = 'down'
+                # creates an empty list then adds each slected item
                 names = []
                 for item in self.selected_items:
                     names.append(item.name)
-                self.status_text = "Hiring: {} for ${}".format(','.join(names), clicked_item.cost)
+                    new_cost += item.cost
+                self.status_text = "Hiring: {} for ${}".format(','.join(names), clicked_item.cost + new_cost)
             else:
                 self.status_text = "Hiring: no items for $0.00"
         elif self.mode == RETURN_MODE:
             if clicked_item.name not in self.selected_items and clicked_item.status == 'out':
                 self.selected_items.append(clicked_item)
-                #set instance state
-                instance.state = 'down'
-                self.status_text = "Returning: {}".format(name)
+                # creates an empty list then adds each slected item
+                names = []
+                for item in self.selected_items:
+                    names.append(item.name)
+                self.status_text = "Returning: {}".format(','.join(names))
 
+    def press_list(self):
+        """
+        :return:
+        """
+        # Sets the mode the instance is in
+        self.mode = LIST_MODE
+        # changes state of each button
+        self.root.ids.hireItems.state = 'normal'
+        self.root.ids.listItems.state = 'down'
+        self.root.ids.returnItems.state = 'normal'
 
     def press_hire(self):
         """
-
+        Puts the user into hire mode and changes states of each button
         :return:
         """
+
         self.mode = HIRE_MODE
-        #changes state of each button
+        # changes state of each button
         self.root.ids.hireItems.state = 'down'
         self.root.ids.listItems.state = 'normal'
         self.root.ids.returnItems.state = 'normal'
 
-
     def press_return(self):
         """
+        Puts the user into return mode and changes states of each button
         :return:
         """
+        # Sets the mode the instance is in
         self.mode = RETURN_MODE
-        #changes state of each button
+        # changes state of each button
         self.root.ids.hireItems.state = 'normal'
         self.root.ids.listItems.state = 'normal'
         self.root.ids.returnItems.state = 'down'
 
-
     def press_confirm(self):
         """
+        Changes the selected item/s to in or out state
         :return:
         """
-        return self.selected_items
-
-
+        if self.mode == HIRE_MODE:
+            names = []
+            for item in self.selected_items:
+                names.append(item.name)
+                item.status = 'out'
+                self.status_text = "{} are now out.".format(','.join(names))
+        elif self.mode == RETURN_MODE:
+            names = []
+            for item in self.selected_items:
+                names.append(item.name)
+                item.status = 'in'
+                self.status_text = "{} are now back in.".format(','.join(names))
 
     def press_clear(self):
         """
@@ -161,14 +187,13 @@ class EquipmentHire(App):
             instance.state = 'normal'
         self.status_text = ""
 
-    def press_save(self, added_name, added_number):
+    def press_save(self, added_name, added_description, added_priceperday):
         """
         Handler for pressing the save button in the add entry popup - save a new entry to memory
-        :param added_name: name text input (from popup GUI)
-        :param added_number: phone number text input (string)
+        :param ?
         :return: None
         """
-        self.itemlist[added_name] = added_number
+        self.itemlist[added_name] = added_priceperday
         # change the number of columns based on the number of entries (no more than 5 rows of entries)
         self.root.ids.entriesBox.cols = len(self.itemlist) // 5 + 1
         # add button for new entry (same as in create_entry_buttons())
@@ -178,6 +203,10 @@ class EquipmentHire(App):
         # close popup
         self.root.ids.popup.dismiss()
         self.clear_fields()
+        # try:
+        #     pass
+        # except:
+        #     pass
 
     def clear_fields(self):
         """
@@ -185,8 +214,9 @@ class EquipmentHire(App):
         If we don't do this, the popup will still have text in it when opened again
         :return: None
         """
-        self.root.ids.addedName.text = ""
-        self.root.ids.addedNumber.text = ""
+        self.root.ids.addedItemName.text = ""
+        self.root.ids.addedDescription.text = ""
+        self.root.ids.addedPricePerDay.text = ""
 
     def press_cancel(self):
         """
@@ -196,7 +226,6 @@ class EquipmentHire(App):
         self.root.ids.popup.dismiss()
         self.clear_fields()
         self.status_text = ""
-
 
 
 EquipmentHire().run()
